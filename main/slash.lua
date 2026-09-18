9a9bf4afdeae2a6738e3991192f721673a381eea
hystrixmitis <hystrixmitis@proton.me>
2026-09-17 23:04:16 -0500

    Renaming the slash commands and Redux-ing comments/text; Fixed issue with the settings that were preventing them from loading/saving.

--- main/slash.lua
+++ main/slash.lua
@@ -4,34 +4,55 @@ local C = rematchRedux.constants
 local settings = rematchRedux.settings
 
 --[[
+    *** Commands documented for end users ***
+    *
+    * /rematchredux               : no arguments, toggles the RematchRedux window
+    * /rematchredux <team name>   : loads a team of the given name; if <team name> is not found, a message is printed to chat
+    *
+    * /redux can also be used as a shortcut for /rematchredux
+    *
+    *** End commands documented for end users ***
 
-    Aside from /rematch <team name> to load a team in a macro, slash commands are for "administrative"
-    tasks that should not be exposed in the UI.
-
-    /rematch <team name>         : loads a team of the given name
-    /rematch targetdata          : generates data for a new target to add to targetData.lua
-    /rematch delete all teams    : wipes all teams and groups
-    /rematch reset everything    : wipes all settings, teams, groups, etc. and restores addon to initial state
+    **********************************************************************************************************
+    *
+    * HERE BE DRAGONS AND NOT GOOD ONES LIKE ALEXSTRASZA!
+    *
+    * The following commands are for development/testing purposes and are not documented in the UI.
+    *
+    * THEY ARE NOT FOR END USERS. They are for addon developers and testers and may be removed or changed at any time.
+    *
+    * IF YOU RUN THESE COMMANDS BECAUSE YOU READ THEM IN THE SOURCE CODE, YOU ARE ON YOUR OWN. YOU WILL LOSE ALL OF YOUR TEAMS, 
+    * SETTINGS, AND/OR DATA. BACKUPS WILL BE YOUR ONLY HOPE. OBI-WAN WILL NOT BE ABLE TO HELP YOU.
+    *
+    * YOU HAVE BEEN WARNED.
+    *
+    * /rematchredux targetdata        : generates data for a new target to add to targetData.lua
+    * /rematchredux delete all teams  : wipes all teams and groups
+    * /rematchredux reset everything  : wipes all settings, teams, groups, etc. and restores addon to initial state
+    * /rematchredux import options    : show a dialog to reset options and update to the ones provided
+    *
+    **********************************************************************************************************
 ]]
 
+SLASH_REMATCHREDUX1 = "/rematchredux"
+SLASH_REMATCHREDUX2 = "/redux"
 
-SLASH_REMATCH1 = "/rematch"
-SlashCmdList["REMATCH"] = function(msg)
+SlashCmdList["REMATCHREDUX"] = function(msg)
     msg = (msg or ""):trim():lower()
 
-    -- "/rematch" with no other command will toggle the rematch window
-    if msg=="" then
+    -- "/rematchredux" with no other command will toggle the RematchRedux window
+    if msg == "" then
         rematchRedux.frame:Toggle()
         return
     end
 
-    -- "/rematch <team name>" will attempt to load a team (if <team name> found)
+    -- "/rematchredux <team name>" will attempt to load a team (if <team name> found)
     if rematchRedux.loadTeam:LoadTeamByName(msg) then
         return -- if LoadTeamByName found a teamID, then it's loading; leave
     end
 
-    -- "/rematch reset all settings" will wipe and settings with a dialog prompt to confirm
-    if msg=="reset everything" then
+    -- "/rematchredux reset everything" will wipe all settings with a dialog prompt to confirm
+    if msg == "reset everything" then
         rematchRedux.dialog:Register("ResetEverything",{
             title = L["Reset Everything"],
             accept = YES,
@@ -41,7 +62,7 @@ SlashCmdList["REMATCH"] = function(msg)
             refreshFunc = function(self,info,subject,firstRun)
                 self.Icon:SetTexture("Interface\\ICONS\\Ability_Creature_Cursed_02")
                 self.Icon:SetTexCoord(0.075,0.925,0.075,0.925)
-                self.Text:SetText(L["This will wipe all settings, teams, queue, etc (absolutely everything), and then reload the UI to start Rematch from scratch."])
+                self.Text:SetText(L["This will wipe all settings, teams, queue, etc (absolutely everything), and then reload the UI to start RematchRedux from scratch."])
                 self.Feedback:Set("warning",L["Warning: This cannot be undone!"])
             end,
             acceptFunc = function(self,info,subject)
@@ -56,8 +77,8 @@ SlashCmdList["REMATCH"] = function(msg)
         return
     end
 
-    -- "/rematch delete all teams" will wipe all teams and groups with a dialog prompt to confirm
-    if msg=="delete all teams" then
+    -- "/rematchredux delete all teams" will wipe all teams and groups with a dialog prompt to confirm
+    if msg == "delete all teams" then
         rematchRedux.dialog:Register("DeleteAllTeams",{
             title = L["Delete All Teams"],
             accept = YES,
@@ -79,12 +100,12 @@ SlashCmdList["REMATCH"] = function(msg)
         return
     end
 
-    -- "/rematch targetdata" will create a new entry for new targets, to add to targetData.lua.
+    -- "/rematchredux targetdata" will create a new entry for new targets, to add to targetData.lua.
     -- To use: Target the target and enter battle (it's ok if you lose target, just don't target
-    -- anything else) and once you're in battle and see opponent pets, enter /rematch targetdata
-    if msg=="targetdata" then
+    -- anything else) and once you're in battle and see opponent pets, enter "/rematchredux targetdata"
+    if msg == "targetdata" then
         if not C_PetBattles.IsInBattle() or not rematchRedux.targetInfo.recentTarget then
-            rematchRedux.utils:Write(L["Usage: Target an npc to create data for, enter a pet battle, and once in battle with opponent pets displayed, enter:\n\124cffffffff/rematch targetdata"])
+            rematchRedux.utils:Write(L["Usage: Target an npc to create data for, enter a pet battle, and once in battle with opponent pets displayed, enter:\n\124cffffffff/rematchredux targetdata"])
             return
         end
         local npcID = rematchRedux.targetInfo.recentTarget
@@ -107,7 +128,7 @@ SlashCmdList["REMATCH"] = function(msg)
             end
         end
         -- close off line
-        result=result..format("}, -- %s, %s",mapName,npcName)
+        result = result..format("}, -- %s, %s",mapName,npcName)
         -- send result to TinyPad if enabled
         if TinyPad then
             TinyPad.Insert(result)
@@ -119,8 +140,8 @@ SlashCmdList["REMATCH"] = function(msg)
         return
     end
 
-    -- "/rematch import options" will show a dialog to reset options and update to the ones provided
-    if msg=="import options" then
+    -- "/rematchredux import options" will show a dialog to reset options and update to the ones provided
+    if msg == "import options" then
         rematchRedux.dialog:ShowDialog("ImportOptions")
         return
     end
